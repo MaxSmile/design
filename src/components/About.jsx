52dbe0789ab9a1c33f54343d626fda1fab0adaa0
Maxim Vasilkov <maxim.vasilkov@gmail.com>
2024-05-07 11:02:52 +0930

    about Iryna Vasylkova

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -31,9 +31,20 @@ const AboutMe = () => {
                                 className="hidden lg:block"
                             />
 
-                            <div className="bio-text" dangerouslySetInnerHTML={{__html: about.bio}}/>
+                            <div className="bio-text">
+                                
+                                <p>I'm Irina Vasilkova, the Design Director at Vasilkoff CY Ltd, where I bring ideas to life through captivating design and user-centric experiences.</p>
+                                <p>With a passion for aesthetics and a knack for detail, I thrive on crafting memorable digital journeys. My journey into design began in the world of large format advertising printing, where I learned the importance of making a statement through visual communication. Building on this foundation, I pursued my bachelor's degree in design at the National Academy of Managerial Personnel of Culture and Arts in Kyiv, refining my skills and embracing new possibilities.</p>
+                                <p>At Vasilkoff, I've had the privilege of working on diverse and exciting projects that challenge and inspire me every day. From developing the sleek interface of Easy VPN Free to crafting the elegant WordPress website for Cbay Rent a Car, each endeavor has allowed me to push creative boundaries and deliver exceptional results.</p>
+                                <p>One of my most rewarding experiences was being part of the Heptapolis project, where I contributed to building a comprehensive solution for a crypto ICO project. From smart contract development to landing page design, it was a thrilling journey that showcased the power of collaboration and innovation.</p>
+                                <p>My portfolio also includes projects like Kimfoot, a platform promoting holistic foot treatments, and Coinage Wallet, a cryptocurrency exchange platform that reflects Vasilkoff's expertise and vision.</p>
+                                <p>Whether it's designing intuitive apps like Camera Mirrors or creating impactful websites for corporate clients like Inter-Euro Investments, my goal is always the same: to craft experiences that delight users and drive success for our partners.</p>
+                                <p>I'm incredibly grateful for the opportunity to do what I love every day and to be part of a team that values creativity, innovation, and excellence. Together, we're shaping the future of digital experiences, one design at a time.</p>
+                                <p>Thank you for visiting, and I look forward to sharing more of my journey with you!</p>
 
-                            <SocialMedia className="mt-10"/>
+                            </div>
+
+                            <SocialMedia className="mt-10" />
                         </div>
                     </div>
                 </div>
--- src/components/HeroArea.jsx
+++ src/components/HeroArea.jsx
@@ -11,8 +11,8 @@ const HeroArea = () => {
         >
             <div className="container">
                 <div className="mt-8 font-light">
-                    <h1 className="text-white font-light text-3xl sm:text-[45px] md:text-[55px] leading-none">I'M LUCY DOE</h1>
-                    <h2 className="text-white font-light text-md sm:text-lg tracking-4 mt-2 sm:mt-4">WEB DEVELOPER</h2>
+                    <h1 className="text-white font-light text-3xl sm:text-[45px] md:text-[55px] leading-none uppercase">I'm Iryna Vasylkova</h1>
+                    <h2 className="text-white font-light text-md sm:text-lg tracking-4 mt-2 sm:mt-4">Graphic Designer</h2>
                 </div>
             </div>
 
