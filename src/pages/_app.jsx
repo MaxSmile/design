28589c59d3a93c942266a838fd5813727bcd03ac
Developer Vasilkoff <dev.vasilkoff@gmail.com>
2024-07-23 18:24:16 +0930

    quick fix

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,7 +4,7 @@ const Rokstar = ({Component, pageProps}) => {
     return (
         <>
             <Head>
-                <title>Rokstar :: React Portfolio Template</title>
+                <title>Iryna Vasylkova</title>
             </Head>
             <Component {...pageProps} />
         </>
